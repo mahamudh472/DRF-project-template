f94cb3e4285cf1eadcbc9e79f485eb4a76447757
Md Mahmud Hasan <mahadymahamudh472@gmail.com>
2025-12-12 04:10:38 +0600

    script issue fixed

--- run_me_first.py
+++ run_me_first.py
@@ -12,31 +12,31 @@ os.rename('project_name', project_name)
 
 with open(f'{project_name}/settings.py', 'r') as file:
     settings_content = file.read()
-settings_content = settings_content.replace('core', project_name)
+settings_content = settings_content.replace('project_name', project_name)
 with open(f'{project_name}/settings.py', 'w') as file:
     file.write(settings_content)
 
 with open('manage.py', 'r') as file:
     manage_content = file.read()
-manage_content = manage_content.replace('core', project_name)
+manage_content = manage_content.replace('project_name', project_name)
 with open('manage.py', 'w') as file:
     file.write(manage_content)
 
 with open(f'{project_name}/wsgi.py', 'r') as file:
     wsgi_content = file.read()
-wsgi_content = wsgi_content.replace('core', project_name)
+wsgi_content = wsgi_content.replace('project_name', project_name)
 with open(f'{project_name}/wsgi.py', 'w') as file:
     file.write(wsgi_content)
 
 with open(f'{project_name}/asgi.py', 'r') as file:
     asgi_content = file.read()
-asgi_content = asgi_content.replace('core', project_name)
+asgi_content = asgi_content.replace('project_name', project_name)
 with open(f'{project_name}/asgi.py', 'w') as file:
     file.write(asgi_content)
 
 with open(f'{project_name}/urls.py', 'r') as file:
     urls_content = file.read()
-urls_content = urls_content.replace('core', project_name)
+urls_content = urls_content.replace('project_name', project_name)
 with open(f'{project_name}/urls.py', 'w') as file:
     file.write(urls_content)
 
